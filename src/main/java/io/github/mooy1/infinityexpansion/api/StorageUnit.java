62362bf0cfc886368436609a447e617a9702931b
devJuban <jubanreal500@gmail.com>
2026-08-13 09:23:12 +0100

    add more stuff to api, that hopefully work

--- src/main/java/io/github/mooy1/infinityexpansion/api/StorageUnit.java
+++ src/main/java/io/github/mooy1/infinityexpansion/api/StorageUnit.java
@@ -6,12 +6,47 @@ import org.bukkit.block.Block;
 import org.jetbrains.annotations.NotNull;
 
 public class StorageUnit {
-    public int getAmountOfItems(@NotNull Block block){
-        Location l = block.getLocation();
+
+    public int getAmountOfItems(@NotNull Block b){
+        Location l = b.getLocation();
         return Integer.parseInt(BlockStorage.getLocationInfo(l, "stored"));
     }
 
     public int getAmountOfItems(@NotNull Location l){
         return Integer.parseInt(BlockStorage.getLocationInfo(l, "stored"));
+    }
+
+    public void withdrawFromUnit(@NotNull Block b, int amount){
+        int currentAmount = getAmountOfItems(b);
+        if (amount > currentAmount) return;
+        BlockStorage.addBlockInfo(b, "stored", String.valueOf(currentAmount - amount));
+    }
+
+    public void withdrawFromUnit(@NotNull Location l, int amount){
+        int currentAmount = getAmountOfItems(l);
+        if (amount > currentAmount) return;
+        BlockStorage.addBlockInfo(l, "stored", String.valueOf(currentAmount - amount));
+    }
+
+    // Needs working on
+    // requires MAX variable
+//    public void depositToUnit(@NotNull Block b, int amount){
+//        int currentAmount = getAmountOfItems(b);
+//        if (amount > currentAmount) return;
+//        BlockStorage.addBlockInfo(b, "stored", String.valueOf(currentAmount + amount));
+//    }
+//
+//    public void depositToUnit(@NotNull Location l, int amount){
+//        int currentAmount = getAmountOfItems(l);
+//        if (amount > currentAmount) return;
+//        BlockStorage.addBlockInfo(l, "stored", String.valueOf(currentAmount + amount));
+//    }
+
+    public void emptyUnit(@NotNull Block b){
+        BlockStorage.addBlockInfo(b, "stored", "0");
+    }
+
+    public void emptyUnit(@NotNull Location l){
+        BlockStorage.addBlockInfo(l, "stored", "0");
     }
 }
